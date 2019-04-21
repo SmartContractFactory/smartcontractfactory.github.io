679dba5dfda74d014b6b5a36ab818491bb4f31b8
Zenos Pavlakou <zenospavlakou@outlook.com>
2019-04-21 02:31:25 +0100

    Update tableCopy.js

--- tutorial/js/tableCopy.js
+++ tutorial/js/tableCopy.js
@@ -1,37 +1,3 @@
-// var copyBtn1 = document.querySelector('#copy_200_addrs');
-// copyBtn1.addEventListener('click', function () {
-//   var urlField1 = document.querySelectorAll('table')[0];
-   
-//   // create a Range object
-//   var range1 = document.createRange();  
-//   console.log(range1)
-//   // set the Node to select the "range"
-//   range1.selectNode(urlField1);
-//   // add the Range to the set of window selections
-//   window.getSelection().addRange(range1);
-   
-//   // execute 'copy', can't 'cut' in this case
-//   document.execCommand('copy');
-// }, false);
-
-
-// var copyBtn2 = document.querySelector('#copy_200_vals');
-// copyBtn2.addEventListener('click', function () {
-//   var urlField2 = document.querySelectorAll('table')[1];
-   
-//   // create a Range object
-//   var range2 = document.createRange();  
-//   console.log(range2)
-//   // set the Node to select the "range"
-//   range2.selectNode(urlField2);
-//   // add the Range to the set of window selections
-//   window.getSelection().addRange(range2);
-   
-//   // execute 'copy', can't 'cut' in this case
-//   document.execCommand('copy');
-// }, false);
-
-
 function copyTableData(index) {
 	if (window.getSelection) {
 	   if (window.getSelection().empty) {  // Chrome
